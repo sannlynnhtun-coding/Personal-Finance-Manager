52b6c3e9405819d509c1fcfef2439b7ac2c1715e
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-03-22 17:02:03 +0630

    [Added] Dashboard Service

---
+++ PersonalFinanceManager.Services/Features/Dashboard/DashboardService.cs
@@ -0,0 +1,25 @@
+﻿using PersonalFinanceManager.Query;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceManager.Services.Features
+{
+    public class DashboardService
+    {
+        private readonly DapperService _dapperService;
+
+        public DashboardService()
+        {
+            _dapperService = new DapperService();
+        }
+
+        public decimal Balance()
+        {
+            return _dapperService.QueryFirstOrDefault<decimal>(SqlQuery.Dashboard.Balance, CommandType.StoredProcedure);
+        }
+    }
+}

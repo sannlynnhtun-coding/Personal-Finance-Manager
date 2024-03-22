943ff6ed7b7ff5aabb972e7bf36d052f108728f8
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-03-22 16:58:25 +0630

    [Added] Console App

---
+++ PersonalFinanceManager.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using PersonalFinanceManager.Services.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceManager.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.");
+
+            DashboardService dashboardService = new DashboardService();
+            var balance = dashboardService.Balance();
+            Console.WriteLine(balance);
+
+            Console.ReadKey();
+        }
+    }
+}

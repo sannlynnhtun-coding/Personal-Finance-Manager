476c9efc007989d7c445079abc782ae1bc586ba0
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-03-24 02:08:01 +0630

    [Remove] Old Images

--- PersonalFinanceManager/Properties/Resources.Designer.cs
+++ PersonalFinanceManager/Properties/Resources.Designer.cs
@@ -93,29 +93,9 @@ namespace PersonalFinanceManager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap icons8_fund_50 {
-            get {
-                object obj = ResourceManager.GetObject("icons8_fund_50", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap icons8_refresh_20 {
             get {
                 object obj = ResourceManager.GetObject("icons8_refresh_20", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap icons8_saving_50 {
-            get {
-                object obj = ResourceManager.GetObject("icons8_saving_50", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
